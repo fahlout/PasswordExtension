2dae25065bccb6f22ce15b88f8854c661c1f1159
Niklas Fahl <niklas.fahl@ua.edu>
2017-10-20 11:26:22 -0500

    - implemented web view filling

--- PasswordExtension/Classes/PasswordExtensionResponse.swift
+++ PasswordExtension/Classes/PasswordExtensionResponse.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public enum PasswordExtensionResponse {
-    case loginSuccess(loginDetails: PasswordExtensionLoginDetails)
+    case loginSuccess(loginDetails: PasswordExtensionLoginDetails, loginDict: [String: Any])
     case success(success: Bool)
     case error(error: Error)
 }
